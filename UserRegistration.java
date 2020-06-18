076aa04e9e8f562e1c2177aae2ca9af151ec270b
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-19 02:24:39 +0530

    REFACTORED logic for all email samples

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,18 +1,38 @@
-import java.util.Scanner;
-
-public class RuleFour 
-{
-	public static boolean patternMatching( String pattern ) 
+public class EmailSamples 		
+{		
+	public boolean email(String email) 
 	{
-	   return pattern.matches( "([ 0-9{1,} A-Z{1,} ?=.*[@#$%^&+=]{1}  a-z  ]{7,})$" );
+	   return email.matches("^([a-z][a-z 0-9 \\ ]*[@][a-z]*[.][a-z . ]*)$");
 	}
-	   
+		   
 	public static void main( String[] args ) 
 	{
-	   Scanner scan = new Scanner(System.in);	
-	   		   
-	   System.out.println("Enter the pattern");
-	   String pattern = scan.nextLine();
-       System.out.println(patternMatching(pattern));
-	}
+		EmailSamples emailSample = new EmailSamples();
+		
+	   System.out.println(emailSample.email("abc@yahoo.com"));	
+	   System.out.println(emailSample.email("abc-100@yahoo.com"));
+	   System.out.println(emailSample.email("abc.100@yahoo.com"));
+	   System.out.println(emailSample.email("abc111@abc.com"));
+	   System.out.println(emailSample.email("abc-100@abc.net"));
+	   System.out.println(emailSample.email("abc.100@abc.com.au"));
+	   System.out.println(emailSample.email("abc@1.com"));
+	   System.out.println(emailSample.email("abc@gmail.com.com"));
+	   System.out.println(emailSample.email("abc+100@gmail.com"));
+	   
+	   System.out.println("\n");
+	   
+	   System.out.println(emailSample.email("abc"));
+	   System.out.println(emailSample.email("abc@.com.my"));
+	   System.out.println(emailSample.email("abc123@gmail.a"));
+	   System.out.println(emailSample.email("abc123@.com"));
+	   System.out.println(emailSample.email("abc123@.com.com"));
+	   System.out.println(emailSample.email(".abc@abc.com"));
+	   System.out.println(emailSample.email(".abc()*@gmail.com"));
+	   System.out.println(emailSample.email("abc@%*.com"));
+	   System.out.println(emailSample.email("abc..2003@gmail.com"));
+	   System.out.println(emailSample.email("abc.@gmail.com"));
+	   System.out.println(emailSample.email("abc@abc@gmail.com"));
+	   System.out.println(emailSample.email("abc@gmail.com.1a"));
+	   System.out.println(emailSample.email("abc@gmail.com.aa.au"));	
+	} 
 }
