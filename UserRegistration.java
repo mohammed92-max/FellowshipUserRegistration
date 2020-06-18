32f418cc3e4326077eb2dc7a470f98fb44fc825d
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-19 01:48:40 +0530

    ADDED logic for valid email

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 
-public class ValidLastName 
+public class ValidEmail 
 {
 	public static boolean patternMatching( String pattern ) 
 	{
-	   return pattern.matches( "^([A-Z]{1}[a-z]{2,})$" );
+	   return pattern.matches( "^([a-z][a-z 0-9 \\ ]*[@][a-z]*[.][a-z . ]*)$" );
 	}
 	   
 	public static void main( String[] args ) 
 	{
 	   Scanner scan = new Scanner(System.in);	
 	   		   
-	   System.out.println("\nEnter the pattern");
+	   System.out.println("Enter the pattern");
 	   String pattern = scan.nextLine();
-      System.out.println(patternMatching(pattern));
+       System.out.println(patternMatching(pattern));
 	}
 }
-
-
-
-
